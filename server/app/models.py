64aa2f394170396beb09d19153d6cccc7274e4a5
Elvira Ramírez <elvirarp92@gmail.com>
2020-10-29 12:25:50 +0100

    define models

---
+++ server/app/models.py
@@ -0,0 +1,13 @@
+from app import app
+
+class user(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(32), nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(128), nullable=False)
+  company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+
+class company(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), nullable=False)
+  users = db.relationship('User', backref='company', lazy=True)
