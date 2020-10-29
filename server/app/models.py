175d03f0afab58f4eeb5978b45433bdb4b1b7928
Elvira Ramírez <elvirarp92@gmail.com>
2020-10-29 13:07:32 +0100

    Correct password length in User model
    Updated length to 512 bits to fit a SHA512 hash

--- server/app/models.py
+++ server/app/models.py
@@ -4,7 +4,7 @@ class user(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(32), nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(128), nullable=False)
+  password = db.Column(db.String(512), nullable=False)
   company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
 
 class company(db.Model):
