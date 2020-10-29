91b03f1c32015e5b0ac824442b1da8d2afdf5a48
Elvira Ramírez <elvirarp92@gmail.com>
2020-10-29 15:35:25 +0100

    create /users POST route

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from os import environ
+from hmac import digest
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 from app import routes
+from app import models
+
+if __name__ == '__main__':
+   db.create_all()
+   app.run(debug = True)
