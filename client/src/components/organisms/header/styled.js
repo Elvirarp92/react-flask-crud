6eb3ece73704b55b566a3dbe8fb4ac31d0b591ae
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-02 18:55:37 +0100

    style header

--- client/src/components/organisms/header/styled.js
+++ client/src/components/organisms/header/styled.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
+import { theme } from './../../../utils/constants'
 
 const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  border-bottom: 1px solid ${(props) => props.theme.carbon};
 `
+
+HeaderWrapper.defaultProps = { theme: theme }
 
 export { HeaderWrapper }
