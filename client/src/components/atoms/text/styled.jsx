b3955b0c654f2f2ced889566d25503a4ba814c70
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-02 13:11:10 +0100

    correct text display on TextBold

--- client/src/components/atoms/text/styled.jsx
+++ client/src/components/atoms/text/styled.jsx
@@ -2,16 +2,12 @@ import './../../../utils/fonts.css'
 import styled from 'styled-components'
 
 const Text = styled.p`
-  font-family: Poppins, sans-serif;
+  font-family: 'Poppins', sans-serif;
   line-height: 150%;
   font-weight: 200;
-
 `
-const TextBold = styled.p`
-
+const TextBold = styled(Text)`
   font-weight: 400;
-
 `
-
 
 export { Text, TextBold }
