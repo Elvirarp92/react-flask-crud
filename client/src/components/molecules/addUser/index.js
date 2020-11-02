04bfcf2b3b329246882d9331d1fee6dc24e537ec
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-02 18:45:14 +0100

    create AddUser button (that is gonna need extra logic)

---
+++ client/src/components/molecules/addUser/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from './../../atoms'
+
+const AddUser = ({ action }) => {
+  return (<Button>Añadir</Button>)
+}
+
+export { AddUser }
--- client/src/components/molecules/index.js
+++ client/src/components/molecules/index.js
@@ -1,1 +1,2 @@
 export { ListElement } from './listElement/index'
+export { AddUser } from './addUser/'
