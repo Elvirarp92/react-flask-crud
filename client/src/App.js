75fd576ba80cf7dccc7b47b56ee8842f9eff3a39
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-04 13:46:41 +0100

    better error handling

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,18 @@ function App() {
   const cancelTokenSource = axios.CancelToken.source()
 
   const [users, setUsers] = useState([])
+  const [err, setError] = useState(undefined)
 
   useEffect(() => {
     axios
       .get('http://127.0.0.1:5000/users', { cancelToken: cancelTokenSource.token })
       .then((response) => setUsers(response.data.users))
-      .catch((err) => console.log(err))
+      .catch((err) => {
+        if (axios.cancelToken(err)) {
+          console.log('call cancelled')
+        }
+        setError(err)
+      })
   }, [])
 
   return <HomePage users={users}></HomePage>
