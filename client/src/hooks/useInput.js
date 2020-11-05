69d0882ebeb101f336aade59332767a55637c604
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-05 18:58:33 +0100

    custom UseInput hook for form handling

---
+++ client/src/hooks/useInput.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+  return [value, handleChange]
+}
+
+export { useInput }
