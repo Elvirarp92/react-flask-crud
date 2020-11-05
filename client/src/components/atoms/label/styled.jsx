2fb0c881f15e29eb0c11bf9485a94fdca248eb91
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-05 18:16:46 +0100

    Create Label atom
    Labels for form inputs!

---
+++ client/src/components/atoms/label/styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import './../../../utils/fonts.css'
+import { theme } from './../../../utils/constants'
+
+const Label = styled.label`
+  font-family: Poppins, sans-serif;
+  color: ${(props) => props.theme.carbon};
+`
+
+Label.defaultProps = {
+  theme: theme,
+}
+
+export { Label }
