175d41ba7acbd9f7e5710855e9a4d82a4537f490
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-05 13:35:51 +0100

    add key to UserList

--- client/src/components/organisms/userList/index.js
+++ client/src/components/organisms/userList/index.js
@@ -6,7 +6,7 @@ const UserList = ({ users }) => {
   return (
     <>
       { users.map((user) => (
-        <ListElement key={user.id} name={user.username} company={user.company} />
+        <ListElement key={user.id} id={user.id} name={user.username} company={user.company} />
       ))}
     </>
   )
