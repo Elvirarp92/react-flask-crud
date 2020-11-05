79a6c8443bd870e8eee8eae81186ec42d0ab3121
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-05 18:57:46 +0100

    restyle Label

--- client/src/components/atoms/label/styled.jsx
+++ client/src/components/atoms/label/styled.jsx
@@ -3,6 +3,7 @@ import './../../../utils/fonts.css'
 import { theme } from './../../../utils/constants'
 
 const Label = styled.label`
+  display: block;
   font-family: Poppins, sans-serif;
   color: ${(props) => props.theme.carbon};
 `
