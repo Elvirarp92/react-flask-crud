c623b82e3e6f428a4d6a9a71bad43e3eb525b5ba
Elvira Ramírez <elvirarp92@gmail.com>
2020-11-05 13:34:10 +0100

    Add Axios call to delete database entries (WIP)
    WARNING: This does not update the DOM. Hence, deleting an entry has no visual feedback. This is to be fixed urgently.

--- client/src/components/molecules/listElement/index.js
+++ client/src/components/molecules/listElement/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import axios from 'axios'
 import { Text, TextBold, Button, ButtonDelete } from './../../atoms'
 import { Container } from './styled'
 
-const ListElement = ({ name, company }) => {
+const ListElement = ({ id, name, company }) => {
+
+  const deleteUser = () => {
+    axios.delete(`http://127.0.0.1:5000/users/${id}`)
+  }
+
   return (
     <Container>
       <TextBold>{name}</TextBold>
@@ -10,7 +16,7 @@ const ListElement = ({ name, company }) => {
       <Container>
         <Button>Detalles</Button>
         <Button>Editar</Button>
-        <ButtonDelete>Borrar</ButtonDelete>
+        <ButtonDelete onClick={deleteUser}>Borrar</ButtonDelete>
       </Container>
     </Container>
   )
